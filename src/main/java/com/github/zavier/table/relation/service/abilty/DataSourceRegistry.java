30fa5a698efe8b4f990c51360889c6fa9d0f2383
zheng <765324639@qq.com>
2025-02-02 09:58:35 +0800

    数据库连接池配置调整

--- src/main/java/com/github/zavier/table/relation/service/abilty/DataSourceRegistry.java
+++ src/main/java/com/github/zavier/table/relation/service/abilty/DataSourceRegistry.java
@@ -3,6 +3,7 @@ package com.github.zavier.table.relation.service.abilty;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.PreDestroy;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -18,10 +19,7 @@ public class DataSourceRegistry {
     private Map<String, DataSource> dataSourceMap = new HashMap<>();
 
     public void addDataSource(String name, String url, String username, String password) {
-        final HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(url);
-        hikariConfig.setUsername(username);
-        hikariConfig.setPassword(password);
+        final HikariConfig hikariConfig = buildHikariConfig(url, username, password);
 
         final HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
         final DataSource dataSource = dataSourceMap.putIfAbsent(name, hikariDataSource);
@@ -29,6 +27,24 @@ public class DataSourceRegistry {
             log.error("DataSource already exists: {}", name);
             throw new RuntimeException("DataSource already exists: " + name);
         }
+    }
+
+    private static @NotNull HikariConfig buildHikariConfig(String url, String username, String password) {
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+
+        // 设置连接池参数
+        hikariConfig.setConnectionTimeout(30 * 1000);  // 30秒
+        hikariConfig.setIdleTimeout(0);     // 闲置连接永不回收
+        hikariConfig.setMaxLifetime(30 * 60 * 1000);    // 30分钟
+        hikariConfig.setMinimumIdle(1);          // 最小空闲连接数
+        hikariConfig.setMaximumPoolSize(10);     // 最大连接数
+        hikariConfig.setValidationTimeout(5000); // 5秒
+        hikariConfig.setConnectionTestQuery("SELECT 1"); // MySQL保活语句
+        hikariConfig.setKeepaliveTime(5 * 60 * 1000);    // 每5分钟发送一次保活查询
+        return hikariConfig;
     }
 
     public List<String> getAllSchema() {
